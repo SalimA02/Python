3ef9e1ff4ffeffa6f156f2b8ae43b95b3aa38c93
SalimA02 <52701638+SalimA02@users.noreply.github.com>
2024-07-26 22:19:04 +0100

    Update QuizUnitTest.py

--- PythonQuiz/QuizUnitTest.py
+++ PythonQuiz/QuizUnitTest.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch
-from quiz5 import load_questions, ask_question, check_answer, display_score
+from Quiz import load_questions, ask_question, check_answer, display_score
 import json
 import os
 
